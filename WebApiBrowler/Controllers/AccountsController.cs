edcaf17051f7c3b8a017358e29dbe6b391704f45
hrannar <hrannar@motus.is>
2018-02-15 16:31:43 +0000

    added delete, get, put for users in account

--- WebApiBrowler/Controllers/AccountsController.cs
+++ WebApiBrowler/Controllers/AccountsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -18,7 +20,10 @@ namespace WebApiBrowler.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly IMapper _mapper;
 
-        public AccountsController(UserManager<AppUser> userManager, IMapper mapper, ApplicationDbContext appDbContext)
+        public AccountsController(
+            UserManager<AppUser> userManager, 
+            IMapper mapper, 
+            ApplicationDbContext appDbContext)
         {
             _userManager = userManager;
             _mapper = mapper;
@@ -52,5 +57,98 @@ namespace WebApiBrowler.Controllers
 
             return new OkObjectResult("Account created");
         }
+
+        /// <summary>
+        /// Get all users.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("[controller]/")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(List<Customer>))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest)]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_userManager.Users);
+        }
+
+        /// <summary>
+        /// Get user by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("[controller]/{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(Object))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
+        public IActionResult GetById(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = _userManager.Users.FirstOrDefault(i => Guid.Parse(i.Id) == id);
+
+            return Ok(user);
+        }
+
+        /// <summary>
+        /// Updates user with given id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="userDto"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("[controller]/{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, typeof(string))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Update(Guid id, [FromBody]UserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+           
+            try
+            {
+                var user = _userManager.Users.FirstOrDefault(i => Guid.Parse(i.Id) == id);
+
+                if (user == null) return BadRequest("No user to update");
+
+                user.FacebookId = userDto.FacebookId;
+                user.FirstName = userDto.FirstName;
+                user.LastName = userDto.LastName;
+                user.PictureUrl = userDto.PictureUrl;
+
+                // save 
+                _userManager.UpdateAsync(user);
+
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Deletes a specific user.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("[controller]/{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized)]
+        public IActionResult Delete(Guid id)
+        {
+            var user = _userManager.Users.FirstOrDefault(i => Guid.Parse(i.Id) == id);
+
+            _userManager.DeleteAsync(user);
+            return Ok();
+        }
+
     }
 }
---
+++ WebApiBrowler/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiBrowler.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long? FacebookId { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
